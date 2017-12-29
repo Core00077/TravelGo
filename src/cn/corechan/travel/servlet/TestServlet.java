bd2551d8e3985cb7b69cc8ab6d51da140c04b6c1
Core <core00077@qq.com>
2017-12-29 16:21:11 +0800

    超开心学会了读form-data表单的东西，用的apache common upload的包，接着赶紧把发布商品和提交认证的图片上传接口合并起来。然后跑去写博客hhhhhhh

--- src/cn/corechan/travel/servlet/TestServlet.java
+++ src/cn/corechan/travel/servlet/TestServlet.java
@@ -1,12 +1,20 @@
 package cn.corechan.travel.servlet;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/test")
 public class TestServlet extends HttpServlet {
@@ -15,10 +23,44 @@ public class TestServlet extends HttpServlet {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("UTF-8");
-        Enumeration<String> names=req.getParameterNames();
-        while (names.hasMoreElements()){
-            String name=names.nextElement();
-            resp.getWriter().println(name+"\t"+req.getParameter(name));
+        System.out.println(req.getContentType());
+        Enumeration<String> parameterNames = req.getParameterNames();
+        resp.getWriter().println("------这是parameter部分------");
+        while (parameterNames.hasMoreElements()) {
+            String name = parameterNames.nextElement();
+            resp.getWriter().println(name + "\t" + req.getParameter(name));
+        }
+        boolean isMultipart = ServletFileUpload.isMultipartContent(req);
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        factory.setSizeThreshold(1024 * 1024);
+        factory.setRepository(new File("testcore"));
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        if (isMultipart) {
+            try {
+                List<FileItem> items = upload.parseRequest(req);
+                resp.getWriter().println("------这是form-data部分------");
+                for (FileItem item : items) {
+                    if (item.isFormField()) {
+                        String name = item.getFieldName();
+                        String value = item.getString();
+                        resp.getWriter().print(name + "\t" + value);
+                    } else {
+                        String name = item.getName();
+                        String field = item.getFieldName();
+                        long size = item.getSize();
+                        String type = item.getContentType();
+                        String url = "/testcore/"+name;
+                        resp.getWriter().println(field + "\t" + type + "\t" + size + "kb\t" + url);
+                        File file = new File(getServletConfig().getServletContext().getRealPath("testcore"), name);
+                        if (!file.exists())
+                            file.getParentFile().mkdir();
+                        item.write(file);
+                        System.out.println(file.getAbsolutePath());
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
